efb98e87c98399af6cb6c4b043856fa9cea02b1d
James Obilor <89047128+ChinememmaObilor29@users.noreply.github.com>
2024-03-05 09:51:14 +0100

    Update testimonials.js

--- components/testimonials.js
+++ components/testimonials.js
@@ -11,7 +11,7 @@ const Testimonials  = () => {
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
         <div className="lg:col-span-2 xl:col-auto">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-gray-100">
             <p className="text-2xl leading-normal ">
             Explore our Professional <Mark>Printing</Mark>Services for Crisp, Clear, and Convenient Results
             </p>
@@ -24,7 +24,7 @@ const Testimonials  = () => {
           </div>
         </div>
         <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-gray-100">
             <p className="text-2xl leading-normal ">
             Seamlessly Navigate <Mark>Passport</Mark>Services with Our Dedicated Support!
             </p>
@@ -37,7 +37,7 @@ const Testimonials  = () => {
           </div>
         </div>
         <div className="">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
+          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-gray-100">
             <p className="text-2xl leading-normal ">
             Access Hassle-Free Online <Mark>Application</Mark>Services with Ease and Efficiency!
             </p>
@@ -68,7 +68,7 @@ function Avatar(props) {
       </div>
       <div>
         <div className="text-lg font-medium">{props.name}</div>
-        <div className="text-gray-600 dark:text-gray-400">{props.title}</div>
+        <div className="text-gray-600 dark:text-gray-600">{props.title}</div>
       </div>
     </div>
   );
@@ -78,7 +78,7 @@ function Mark(props) {
   return (
     <>
       {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
+      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:bg-indigo-100 dark:bg-indigo-100 dark:text-indigo-800">
         {props.children}
       </mark>{" "}
     </>
