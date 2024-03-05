dcef7b3cfcc6c713c8635aa3eaa0d884de9b3539
James Obilor <89047128+ChinememmaObilor29@users.noreply.github.com>
2024-03-05 09:57:26 +0100

    Update testimonials.js

--- components/testimonials.js
+++ components/testimonials.js
@@ -78,7 +78,7 @@ function Mark(props) {
   return (
     <>
       {" "}
-      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:bg-indigo-100 dark:bg-indigo-100 dark:text-indigo-800">
+      <mark className="text-indigo-800 bg-indigo-100 rounded-md ring-indigo-100 ring-4 dark:ring-indigo-900 dark:bg-indigo-900 dark:text-indigo-200">
         {props.children}
       </mark>{" "}
     </>
