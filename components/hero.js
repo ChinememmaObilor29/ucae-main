819e20880f12e817e5370bdbd7d0017b11914245
James Obilor <89047128+ChinememmaObilor29@users.noreply.github.com>
2024-03-05 17:36:01 +0100

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Container from "./container";
 import heroImg from "../public/img/businessman-taking-note.jpg";
-import mobileHeroImg from "../public/img/ai-hp.jpg";
+import mobileHeroImg from "../public/img/hpwoman.png";
 const Hero = () => {
   return (
     <>
