b99faf4707cbc6959b39b6219fb6fb014050f224
James Obilor <89047128+ChinememmaObilor29@users.noreply.github.com>
2024-05-03 13:32:33 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -92,9 +92,10 @@ export default function Example() {
                   name="country"
                   className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm bg-white"
                 >
-                  <option>US</option>
+                  <option>NG</option>
                   <option>CA</option>
                   <option>EU</option>
+                  <option>US</option>
                 </select>
                 <ChevronDownIcon
                   className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
